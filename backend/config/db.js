f87fe374ac8b489e81d6688375ba5a15e1d2d11e
Mia <mia@Mias-MacBook-Pro.local>
2020-10-05 23:52:47 -0600

    connected db

---
+++ backend/config/db.js
@@ -0,0 +1,21 @@
+// connection file
+
+import mongoose from 'mongoose'
+
+const connectDB = async () => {
+    try {
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            useUnifiedTopology: true,
+            useNewUrlParser: true,
+            useCreateIndex: true
+        })
+
+        console.log(`MongoDB Connected: ${conn.connection.host}`)
+    } catch (error) {
+        console.error(`Error: ${error.message}`)
+        // exit with failure
+        process.exit(1)
+    }
+}
+
+export default connectDB
