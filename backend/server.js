815036a94c77d98bcfd4cd972844dabf85806f19
Mia <mia@Mias-MacBook-Pro.local>
2020-10-07 21:08:55 -0600

    custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import colors from 'colors'
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 import connectDB from './config/db.js'
 
 
@@ -27,6 +28,11 @@ app.get('/', (req, res) => {
 // })
 
 app.use('/api/products', productRoutes)
+
+//Middleware to handle errors
+app.use(notFound)
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT || 5000
 
