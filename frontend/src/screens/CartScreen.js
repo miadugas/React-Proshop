eb2521a978aea51126ace190a2986f3986b2e7d0
Mia <mia@Mias-MacBook-Pro.local>
2020-10-09 19:49:27 -0600

    adding cart screen

--- frontend/src/screens/CartScreen.js
+++ frontend/src/screens/CartScreen.js
@@ -25,7 +25,30 @@ const CartScreen = ({ match, location, history }) => {
       }, [dispatch, productId, qty])
 
 
-    return <div>Cart</div>
+    return  <Row>
+<Col md={8}>
+<h1>Shopping Cart</h1>
+{cartItems.length === 0 ? (
+          <Message>
+            Your cart is empty <Link to='/'>Go Back</Link>
+          </Message>
+        ) : (
+          <ListGroup variant='flush'>
+
+          </ListGroup>
+
+        )}
+
+
+</Col>
+<Col md={2}>
+
+</Col>
+<Col md={2}>
+
+</Col>
+
+    </Row>
     
 }
 
